4a185550ca03cbdffb51c42497e4a5fdbde3ab70
wondongjin <82082770+dongjin97@users.noreply.github.com>
2024-02-07 20:59:11 +0900

    [Fix]파일누락수정중

---
+++ ThefirstForecast /ThefirstForecast/Main/MainVC.swift
@@ -0,0 +1,15 @@
+//
+//  MainVC.swift
+//  ThefirstForecast
+//
+//  Created by 원동진 on 2/5/24.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+}
