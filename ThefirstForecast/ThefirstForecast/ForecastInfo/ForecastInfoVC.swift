f8ad449bc17dae7e70865a5133c3b85df1b7bc93
gyeongeun <club2548@gmail.com>
2024-02-08 22:38:46 +0900

    [Feat] Pull to Refresh 구현 기상요소 최신화를 위해 Pull to Refresh 구현 indicator의 centerYAnchor가 view의topAnchor에 걸려있는 문제 해결 요망

--- ThefirstForecast/ThefirstForecast/ForecastInfo/ForecastInfoVC.swift
+++ ThefirstForecast/ThefirstForecast/ForecastInfo/ForecastInfoVC.swift
@@ -16,17 +16,20 @@ enum WeatherSection {
 class ForecastInfoVC: UIViewController {
     
     let time = ["오전 0시", "오전 3시", "오전 6시", "오전 9시", "오전 12시", "오전 15시", "오전 18시", "오전 21시", "오전 24시"]
+    let weather = ["cloud.sun.fill", "cloud.sun.rain.fill", "cloud.sleet.fill", "sun.max.fill", "sun.rain.fill", "rainbow", "cloud.sun.rain.fill", "cloud.sleet.fill", "sun.max.fill"]
     let temperature = [3, 2, 1, 2, 5, 7, 5, 4, 3]
     
-    let category = ["1체감온도", "2강수량", "3가시거리", "4습도"]
+    let sectionIcon = ["thermometer.medium", "drop.fill", "eye.fill", "humidity"]
+    let category = ["체감온도", "강수량", "가시거리", "습도"]
     let value = [1, 2, 3, 4]
-    let weatherDescription = ["1습도로 인해 체감 온도가 실제 온도보다 더 높게 느껴집니다.", "2이후 수요일에 2mm의 비가 예상됩니다.", "3가시거리가 매우 좋습니다.", "4현재 이슬점이 23°입니다."]
+    let weatherDescription = ["습도로 인해 체감 온도가 실제 온도보다 더 높게 느껴집니다.", "이후 수요일에 2mm의 비가 예상됩니다.", "가시거리가 매우 좋습니다.", "현재 이슬점이 23°입니다."]
     
     
     private let scrollView : UIScrollView = {
         let view = UIScrollView()
         view.backgroundColor = .clear
         view.contentInsetAdjustmentBehavior = .never    // contentView가 navigationBar를 무시하고 constant 잡을 수 있도록 해줌
+        view.refreshControl = UIRefreshControl()
         return view
     }()
     
@@ -80,6 +83,7 @@ class ForecastInfoVC: UIViewController {
         print("#", #function)
         self.view.backgroundColor = .orange
         self.navigationController?.navigationBar.isHidden = true
+        
         let backgroundImage = UIImageView(image: UIImage(named: "backgroundImage"))
         backgroundImage.contentMode = .scaleAspectFill
         view.insertSubview(backgroundImage, at: 0)
@@ -91,6 +95,7 @@ class ForecastInfoVC: UIViewController {
                 backgroundImage.bottomAnchor.constraint(equalTo: view.bottomAnchor)
             ])
         
+        scrollView.refreshControl?.addTarget(self, action: #selector(refreshFunction), for: .valueChanged)
         
         addSubviewsInForecaseInfoVC()
         autoLayoutInForecastInfoVC()
@@ -131,6 +136,11 @@ class ForecastInfoVC: UIViewController {
 }
 
 extension ForecastInfoVC {
+    
+    @objc func refreshFunction() {
+        scrollView.refreshControl?.endRefreshing()
+        
+    }
     
     private func setBlurOfDailyWeatherCollectionView(blurEffect: UIBlurEffect.Style, on thisView: UIView) {
         print(#function)
@@ -186,7 +196,7 @@ extension ForecastInfoVC {
             contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
             contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
             contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
-            contentView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 2.0),
+            contentView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1.4),
             
             currentWeatherView.topAnchor.constraint(equalTo: contentView.topAnchor),
             currentWeatherView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
@@ -216,7 +226,7 @@ extension ForecastInfoVC {
 
 extension ForecastInfoVC : UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        let numOfSection = 5
+        let numOfSection = 1
         if collectionView == dailyWeatherCollectionView {
             return numOfSection
         } else {
@@ -237,19 +247,21 @@ extension ForecastInfoVC : UICollectionViewDataSource {
         if collectionView == dailyWeatherCollectionView {
             let cell = dailyWeatherCollectionView.dequeueReusableCell(withReuseIdentifier: DailyWeatherCollectionViewCell.reuseIdentifier, for: indexPath) as? DailyWeatherCollectionViewCell
             let time = time[indexPath.item]
+            let weather = weather[indexPath.item]
             cell?.setCollectionViewCell(
                 time: time,
-                icon: UIImage(systemName: "cloud.sun.rain") ?? UIImage(),
+                icon: UIImage(systemName: weather)?.withRenderingMode(.alwaysOriginal) ?? UIImage(),
                 temperature: "\(temperature[indexPath.item])°")
             return cell ?? UICollectionViewCell()
         }
         else {
             let cell = otherInfoCollectionView.dequeueReusableCell(withReuseIdentifier: OtherInfoCollectionViewCell.reuseIdentifier, for: indexPath) as? OtherInfoCollectionViewCell
+            let sectionIcon = sectionIcon[indexPath.item]
             let category = category[indexPath.item]
             let value = value[indexPath.item]
             let weatherDescription = weatherDescription[indexPath.item]
             cell?.setOtherInfoCell(
-                icon: UIImage(systemName: "cloud.sun.rain") ?? UIImage(),
+                icon: UIImage(systemName: sectionIcon) ?? UIImage(),
                 title: category,
                 value: String(value),
                 description: weatherDescription)
